275b1c4156f2b8779a0f86df52ab22ee9f33ba47
BOUGET Alexandre <abouget68@gmail.com>
2024-04-01 15:13:52 +0200

    lineup lang

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from pathlib import Path
 
 setup(
-    name="lineup",
+    name="lineup-lang",
     version="0.1.0",
     description="Pseudo language interpreter for Python",
     long_description=Path(__file__).parent.joinpath("README.md").read_text(),
@@ -17,6 +17,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Interpreters",
-
     ]
 )
